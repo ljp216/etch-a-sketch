6a0e560bc2f3225b9b5793d9ce93c96c7014765b
ljp216 <liampenlington@live.co.uk>
2021-04-14 14:28:54 +0100

    allow user to change grid size

--- app.js
+++ app.js
@@ -10,11 +10,24 @@ for (var i = 0; i < 256; i++) {
    
  }
 
-let clear = () => {
+const clear = () => {
     let squares = container.querySelectorAll('div');
     squares.forEach(item => item.style.backgroundColor = '#ffffff')
 }
+const howMany = () => {
+    let gridSize = Number(prompt('How Many?'));
+    container.innerHTML = ''
+    for (var i = 0; i < gridSize * gridSize; i++) {
+        const gridSquare = document.createElement('div')
+        gridSquare.classList = "grid";
+        gridSquare.addEventListener("mouseover", changeColor);
+        container.appendChild(gridSquare);
+       
+     }
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+}
+
 const button = document.querySelector('.reset')
 button.addEventListener('click', clear)
-
+button.addEventListener('click', howMany)
 
